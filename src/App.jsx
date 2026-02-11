8d3274704a825bd5890d6f14e9605390f547279a
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2026-02-11 17:40:17 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.css'
+import MovieCard from "./components/MovieCard"
 
 function App() {
   return (
     <>
-      <Text display="HELLLOW 1"/>
-      <Text display="HELLLOW 2"/>
+      <MovieCard movie={{title: "Harry Potter", release_date: "2016"}}/>
+      <MovieCard movie={{title: "Harry Potter 2", release_date: "2017"}}/>
     </>
-  )
-}
-
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
   )
 }
 
